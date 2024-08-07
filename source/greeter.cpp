cfcacaf9086635a44dd5bc5c2ac64ada55772523
Fred Chen <56397993+fredderf121@users.noreply.github.com>
2024-08-06 21:35:20 -0400

    chore: update devenv to remove code coverage pre-commit hooks (#2)
    Also, some demo changes

--- source/greeter.cpp
+++ source/greeter.cpp
@@ -15,7 +15,7 @@ std::string Greeter::greet(LanguageCode lang) const
   switch (lang)
   {
     default:
-    case LanguageCode::EN: return fmt::format("Hello, {}!", name);
+    case LanguageCode::EN: return fmt::format("Hello bob, {}!", name);
     case LanguageCode::DE: return fmt::format("Hallo {}!", name);
     case LanguageCode::ES: return fmt::format("¡Hola {}!", name);
     case LanguageCode::FR: return fmt::format("Bonjour {}!", name);
--- test/source/greeter.cpp
+++ test/source/greeter.cpp
@@ -9,7 +9,7 @@ TEST_CASE("Greeter")
   using namespace greeter;
 
   const Greeter greeter("Tests");
-  CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
+  CHECK(greeter.greet(LanguageCode::EN) == "Hello bob, Tests!");
   CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
   CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
   CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
